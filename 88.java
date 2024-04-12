778165359871422c773c727fd8cb29a688bc2871
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-04-12 15:05:54 -0400

    goofy but much more optimized for speed, didn't just combine and then sort

--- 88.java
+++ 88.java
@@ -2,9 +2,27 @@ import java.util.Arrays;
 
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i = m; i<(m+n); i++) {
-            nums1[i] = nums2[i-m];
+        int[] tempArray = Arrays.copyOf(nums1, nums1.length);
+        int numAtArray1 = 0, numAtArray2 = 0;
+        for(int i = 0; i<(m+n); i++) {
+            if(numAtArray1 >= m) {
+                nums1[i] = nums2[numAtArray2];
+                numAtArray2++;
+                continue;
+            }
+            else if(numAtArray2 >= n) {
+                nums1[i] = tempArray[numAtArray1];
+                numAtArray1++;
+                continue;
+            }
+            else if(tempArray[numAtArray1] <= nums2[numAtArray2]) {
+                nums1[i] = tempArray[numAtArray1];
+                numAtArray1++;
+            }
+            else {
+                nums1[i] = nums2[numAtArray2];
+                numAtArray2++;
+            }
         }
-        Arrays.sort(nums1);
     }
 }
