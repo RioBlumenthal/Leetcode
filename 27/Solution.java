9be6e81155b62ce48afc62034dee20fc1a989811
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-04-12 16:11:02 -0400

    attempting to make 27, vs code is being buggy as hell

---
+++ 27/Solution.java
@@ -0,0 +1,20 @@
+//moves all elements equal to val to the end of the array
+//and returns how many elements that was
+class Solution {
+   public int removeElement(int[] nums, int val) {
+       int numElementsRemoved = 0;
+       int lastIndex = nums.length - 1;
+       for(int i = 0; i<=lastIndex; i++) {
+           if(nums[i] == val) {    
+               while(nums[lastIndex] == val && lastIndex > i) {
+                   lastIndex--;
+               }
+               numElementsRemoved++;
+               nums[i] = nums[lastIndex];
+               nums[lastIndex] = val;
+               lastIndex--;
+           }
+       }
+       return numElementsRemoved;
+   }
+}
