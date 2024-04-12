48606229af079e17a32011af0418b4bd8f380b6a
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-04-12 16:51:23 -0400

    finished 27

--- 27/Solution.java
+++ 27/Solution.java
@@ -2,20 +2,20 @@
 //and returns how many elements that was
 class Solution {
    public int removeElement(int[] nums, int val) {
-       int numElementsRemoved = 0;
+       int numGoodElements = nums.length;
        int lastIndex = nums.length - 1;
        for(int i = 0; i<=lastIndex; i++) {
            if(nums[i] == val) {    
                while(nums[lastIndex] == val && lastIndex > i) {
                    lastIndex--;
-                   numElementsRemoved++;
+                   numGoodElements--;
                }
-               numElementsRemoved++;
+               numGoodElements--;
                nums[i] = nums[lastIndex];
                nums[lastIndex] = val;
                lastIndex--;
            }
        }
-       return numElementsRemoved;
+       return numGoodElements;
    }
 }
