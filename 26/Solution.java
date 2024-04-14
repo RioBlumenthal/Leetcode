038f42c8eb075e52355ccb391b71c7cc132861ad
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-04-14 17:59:39 -0400

    working but slow solution

--- 26/Solution.java
+++ 26/Solution.java
@@ -1,15 +1,17 @@
 public class Solution {
    public int removeDuplicates(int[] nums) {
       int numUnique = nums.length;
-      for(int i = 1; i < nums.length; i++) {
+      for(int i = 1; i < numUnique; i++) {
          if(nums[i] == nums[i-1]) {
             numUnique--;
-            for(int j = i; j < nums.length; j++) {
-               nums[j-1] = nums[j];
+            for(int j = i; j < nums.length - 1; j++) {
+               nums[j] = nums[j+1];
             }
+            i--;
          }
       }
       return numUnique;
    }
 }
+
 
