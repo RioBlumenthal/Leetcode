6c19d4c81b9a0f94be220f9531f58f3a42f93803
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-04-18 13:22:56 -0400

    time efficient solution

---
+++ interviewTop150/121/Solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        cheapest = prices[0]
+        maxProfit = 0
+        for i in range(1,len(prices)):
+            if prices[i] < cheapest:
+                cheapest = prices[i]
+            elif prices[i] - cheapest > maxProfit:
+                maxProfit = prices[i] - cheapest
+        return maxProfit
