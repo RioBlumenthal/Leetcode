264406f28e5c51535cba5b5ead5db17864215ed3
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-04-23 22:52:08 -0400

    commented code

--- interviewTop150/80/Solution.java
+++ interviewTop150/80/Solution.java
@@ -2,22 +2,19 @@ public class Solution {
    public int removeDuplicates(int[] nums) {
       int length = nums.length;
       if (length <= 2) {
-         return length;
+         return length; // If the length of the array is less than or equal to 2, there are no duplicates to remove, so return the length as it is
       }
       
-      int index = 1;
+      int index = 1; // Initialize the index to 1, as the first element is always unique
       for (int j = 1; j < length; j++) {
-         if (nums[j] != nums[index-1]) {
-            index++;
+         if (nums[j] != nums[index-1]) { // If the current element is not equal to the previous unique element
+            index++; // Increment the index to move to the next unique element
             if (index != j+1) {
-               nums[index] = nums[j];
+               nums[index] = nums[j]; // If the current element is not in its correct position, move it to the correct position
             }
          }
       }
       
-      return index+1;
+      return index+1; // Return the length of the modified array
    }
-}  
-
-
-
+}
