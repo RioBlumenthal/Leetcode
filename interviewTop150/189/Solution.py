1c9e2d8f3a263c5d6ca17903cdac65611f7d709d
Rio <111706275+YargleWithWings@users.noreply.github.com>
2024-04-25 16:17:32 -0400

    commented code

--- interviewTop150/189/Solution.py
+++ interviewTop150/189/Solution.py
@@ -1,5 +1,16 @@
+
 class Solution(object):
-    def rotate(self, nums, k):
-      k = k % len(nums)
-      nums[:] = nums[-k:] + nums[:-k]
-      return nums
+  def rotate(self, nums, k):
+    """
+    Rotates the given list of numbers to the right by k steps.
+
+    Args:
+      nums (List[int]): The list of numbers to be rotated.
+      k (int): The number of steps to rotate the list.
+
+    Returns:
+      List[int]: The rotated list of numbers.
+    """
+    k = k % len(nums) #make sure k is within the range of the list
+    nums[:] = nums[-k:] + nums[:-k] #split the list at k, and then rotate each part
+    return nums
