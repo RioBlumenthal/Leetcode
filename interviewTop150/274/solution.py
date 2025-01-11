aad729d547bb769e038974c1ce231fc11a0ba3d6
Rio <111706275+YargleWithWings@users.noreply.github.com>
2025-01-10 23:57:37 -0600

    leetcode 274 finished with check expects, mediocre time

--- interviewTop150/274/solution.py
+++ interviewTop150/274/solution.py
@@ -1,7 +1,15 @@
-class Solution(object):
-    def hIndex(self, citations):
-        """
-        :type citations: List[int]
-        :rtype: int
-        """
-        
+def hIndex(citations: list[int]) -> int:
+   citations.sort(reverse=True)
+   for i in range(len(citations)):
+      if citations[i] < i+1:
+         return i
+   return len(citations)
+      
+result1 = hIndex([1])
+result2 = hIndex([1, 3, 1])
+result3 = hIndex([1, 3, 1, 3, 3])
+result4 = hIndex([3,3,3])
+assert result1 == 1
+assert result2 == 1
+assert result3 == 3
+assert result4 == 3
